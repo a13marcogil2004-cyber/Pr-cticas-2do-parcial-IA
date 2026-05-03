ad17699d5629bea837cd63d9324d1ab12eea6367
Marco-393 <a13marcogil2004@gmail.com>
2026-05-02 23:03:43 -0600

    Update Búsqueda en Profundidad.py

--- Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Profundidad.py
+++ Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Profundidad.py
@@ -1,1 +1,32 @@
+def dfs_iterativo(grafo, inicio):
+    visitados = set()
+    pila = [inicio]
+    recorrido = []
 
+    while pila:
+        nodo = pila.pop()
+
+        if nodo not in visitados:
+            visitados.add(nodo)
+            recorrido.append(nodo)
+
+            # Se invierte para mantener orden lógico
+            for vecino in reversed(grafo[nodo]):
+                if vecino not in visitados:
+                    pila.append(vecino)
+
+    return recorrido
+
+
+# Grafo de ejemplo
+grafo = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+resultado = dfs_iterativo(grafo, 'A')
+print("Recorrido DFS:", resultado)
