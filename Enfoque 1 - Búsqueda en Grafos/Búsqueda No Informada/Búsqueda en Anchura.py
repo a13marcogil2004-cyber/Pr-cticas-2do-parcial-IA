695825409b5aa54d430004e24c2b4bffa7cdfc95
Marco-393 <a13marcogil2004@gmail.com>
2026-05-02 22:12:50 -0600

    Update Búsqueda en Anchura.py

--- Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Anchura.py
+++ Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Anchura.py
@@ -1,1 +1,10 @@
+from collections import deque
 
+# Grafo representado como diccionario
+grafo = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
