433ad682dec19b58a747a975ae67889e87a5721b
Marco-393 <a13marcogil2004@gmail.com>
2026-05-02 22:23:04 -0600

    Update Búsqueda en Anchura de Costo Uniforme.py

--- Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Anchura de Costo Uniforme.py
+++ Enfoque 1 - Búsqueda en Grafos/Búsqueda No Informada/Búsqueda en Anchura de Costo Uniforme.py
@@ -1,1 +1,17 @@
+import heapq  # cola de prioridad
+
+# Grafo con costos
+grafo = {
+    'A': [('B', 1), ('C', 4)],
+    'B': [('D', 2), ('E', 5)],
+    'C': [('F', 1)],
+    'D': [],
+    'E': [('F', 1)],
+    'F': []
+}
+
+def ucs(grafo, inicio, objetivo):
+    # (costo, nodo, camino)
+    cola = [(0, inicio, [])]
+    visitados = set()
 
