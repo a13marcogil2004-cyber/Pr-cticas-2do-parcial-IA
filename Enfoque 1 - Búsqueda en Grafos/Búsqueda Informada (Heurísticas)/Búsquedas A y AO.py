2922c84443c4dee6536a95e8ebab10371c15f590
Marco-393 <a13marcogil2004@gmail.com>
2026-05-03 18:43:52 -0600

    Update Búsquedas A y AO.py

--- Enfoque 1 - Búsqueda en Grafos/Búsqueda Informada (Heurísticas)/Búsquedas A y AO.py
+++ Enfoque 1 - Búsqueda en Grafos/Búsqueda Informada (Heurísticas)/Búsquedas A y AO.py
@@ -1,4 +1,5 @@
 # Código A*
+
 import heapq
 
 def heuristica(nodo):
@@ -44,3 +45,56 @@ grafo = {
 }
 
 print("A*:", " -> ".join(a_estrella(grafo, 'A', 'D')))
+
+#--------------------------------------------------------------------------------------------------------------
+
+# Código AO*
+
+def ao_estrella(grafo, nodo, heuristica):
+    # Si es nodo final
+    if nodo not in grafo:
+        return nodo
+
+    print(f"Evaluando nodo: {nodo}")
+
+    # Seleccionar la mejor opción
+    mejor_costo = float('inf')
+    mejor_camino = []
+
+    for opcion in grafo[nodo]:
+        costo_total = 0
+        camino_actual = []
+
+        for subnodo in opcion:
+            costo_total += heuristica[subnodo]
+            camino_actual.append(subnodo)
+
+        if costo_total < mejor_costo:
+            mejor_costo = costo_total
+            mejor_camino = camino_actual
+
+    resultado = []
+    for n in mejor_camino:
+        resultado.append(ao_estrella(grafo, n, heuristica))
+
+    return [nodo] + resultado
+
+
+# Grafo AND-OR
+grafo = {
+    'A': [['B'], ['C', 'D']],
+    'B': [['E']],
+    'C': [['F']],
+    'D': [['G']]
+}
+
+heuristica = {
+    'B': 2,
+    'C': 3,
+    'D': 1,
+    'E': 0,
+    'F': 0,
+    'G': 0
+}
+
+print("AO*:", ao_estrella(grafo, 'A', heuristica))
