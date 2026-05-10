ea24f795f2704d54c59bc275b18f8c67df6454ad
Marco-393 <a13marcogil2004@gmail.com>
2026-05-10 16:59:32 -0600

    Update Red Bayesiana.py

---
+++ Enfoque 2/Probabilidad/Red Bayesiana.py
@@ -0,0 +1,43 @@
+prob_lluvia = 0.3
+
+prob_trafico_si_lluvia = 0.8
+prob_trafico_si_no_lluvia = 0.2
+
+def inferencia(trafico):
+
+    if trafico:
+
+        numerador = (
+            prob_trafico_si_lluvia
+            * prob_lluvia
+        )
+
+        denominador = (
+            prob_trafico_si_lluvia
+            * prob_lluvia
+            +
+            prob_trafico_si_no_lluvia
+            * (1 - prob_lluvia)
+        )
+
+    else:
+
+        numerador = (
+            (1 - prob_trafico_si_lluvia)
+            * prob_lluvia
+        )
+
+        denominador = (
+            (1 - prob_trafico_si_lluvia)
+            * prob_lluvia
+            +
+            (1 - prob_trafico_si_no_lluvia)
+            * (1 - prob_lluvia)
+        )
+
+    return numerador / denominador
+
+resultado = inferencia(True)
+
+print("P(Lluvia | Tráfico):")
+print(round(resultado, 4))
